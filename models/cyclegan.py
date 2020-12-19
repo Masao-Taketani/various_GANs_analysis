e4f6804a9b455d2d81885cff02d39d991b33f7e4
Masao-Taketani <taket003@umn.edu>
2020-12-20 03:45:01 +0900

    implementing resnetblock is done

--- models/cyclegan.py
+++ models/cyclegan.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Layer, Conv2D, BatchNormalization, LeakyReLU\
-    Conv2DTranspose, Dropout, concatenate, ZeroPadding2D
+    ReLU, Conv2DTranspose, Dropout, concatenate, ZeroPadding2D
 
 
-def InstanceNormalization(Layer):
+class InstanceNormalization(Layer):
     """
     Instance Normalization Layer (https://arxiv.org/abs/1607.08022).
 
@@ -33,6 +33,40 @@ def InstanceNormalization(Layer):
         inv = tf.math.rsqrt(variance + self.epsilon)
         normalized = (x - mean) * inv
         return self.scale * normalized * self.offset
+
+
+class ResNetBlock(Layer):
+    # Define ResNet block
+
+    def __init__(self, filters, size, strides, padding="VALID", name="resnet_block"):
+        super(ResNetBlock, self).__init__()
+        initializer = tf.random_normal_initializer(0., 0.02)
+        self.kernel_size = kernel_size
+        self.conv2d_1 = Conv2D(filters, 
+                               size, 
+                               strides, 
+                               padding=padding,
+                               kernel_initializer=initializer)
+        self.instance_norm_1 = InstanceNormalization()
+        self.conv2d_2 = Conv2D(num_filters,
+                               kernel_size,
+                               strides,
+                               padding=padding,
+                               kernel_initializer=initializer)
+        self.instance_norm_2 = InstanceNormalization()
+
+        
+    def call(inputs):
+        pad = int((kernel_size - 1) / 2)
+        x = tf.pad(inputs, [[0, 0], [pad, pad], [pad, pad], [0, 0]], "REFLECT")
+        x = self.conv2d_1(x)
+        x = self.instance_norm_1(x)
+        x = ReLU(x)
+        x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]], "REFLECT")
+        x = self.conv2d_2(x)
+        x = self.instance_norm_2(x)
+        return x + inputs
+
 
 
 class Downsample(Layer):
@@ -164,103 +198,6 @@ class Discriminator(Model):
 
 
 
-def get_norm_layer(norm_type):
-    if norm_type.lower() == "batchnorm":
-        return BatchNormalization()
-    elif norm_type.lower() == "instancenorm":
-        return InstanceNormalization()
-    else:
-        raise ValueError("arg `norm_type` has to be either batchnorm "
-                            "or instancenorm")
-
-
-"""
-def downsample(filters, size, norm_type="batchnorm", apply_norm=True):
-    # Conv2D -> BatchNorm(or InstanceNorm) -> LeakyReLU
-
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    result = Sequential()
-    reault.add(Conv2D(filters,
-                      size,
-                      strides=2,
-                      padding="same",
-                      kernel_initializer=initializer,
-                      use_bias=False))
-
-    if apply_norm:
-        if norm_type.lower() == "batchnorm":
-            result.add(BatchNormalization())
-        elif norm_type.lower() == "instancenorm":
-            result.add(InstanceNormalization())
-        else:
-            raise ValueError("arg `apply_norm` has to be either batchnorm or instancenorm")
-
-    result.add(LeakyReLU())
-
-    return result
-
-
-def upsample(filters, size, norm_type="batchnorm", apply_dropout=False):
-    # Conv2DTranspose -> BatchNorm(or InstanceNorm) -> Dropout -> ReLU
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    result = Sequential()
-    result.add(Conv2DTranspose(filters,
-                               size,
-                               strides=2,
-                               padding="same",
-                               kernel_initializer=initializer,
-                               use_bias=False))
-
-    
-    if apply_dropout:
-        result.add(Dropout(0.5))
-
-     result.add(ReLU())
-
-     return result
-
-
-def discriminator(norm_type="batchnorm", target=True):
-    # PatchGAN discriminator is used
-    # target: whether target image is an input or not
-
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    inp = Input(shape=[None, None, 3], name="disc_input_image")
-    x = inp # (bs, 256, 256, channels)
-
-    if target:
-        tar = Input(shape=[None, None, 3], name="target_image")
-        x = concatenate([inp, tar]) # (bs, 256, 256, channels * 2)
-    
-    down1 = downsample(64, 4, norm_type, False)(x) # (bs, 128, 128, 64)
-    down2 = downsample(128, 4, norm_type)(down1) # (bs, 64, 64, 128)
-    down3 = downsample(256, 4, norm_type)(down2) # (bs, 32, 32, 256)
-
-    zero_pad1 = ZeroPadding2D()(down3) # (bs, 34, 34, 256)
-    conv = Conv2D(512,
-                  4,
-                  strides=1,
-                  kernel_initializer=initializer,
-                  use_bias=False)(zero_pad1) # (bs, 31, 31, 512)
-
-    norm1 = check_norm_type(norm_type, conv)
-    leaky_relu = LeakyReLU()(norm1)
-    zero_pad2 = ZeroPadding2D()(leaky_relu) # (bs, 33, 33, 512)
-    last = Conv2D(1,
-                  4,
-                  strides=1,
-                  kernel_initializer=initializer)(zero_pad2) # (bs, 30, 30, 1)
-
-    if target:
-        return Model(inputs=[inp, tar], outputs=last)
-    else:
-        return Model(inputs=inp, outputs=last)
-"""
-
-
 class ResNetGenerator(Model):
     """
     Args:
@@ -314,3 +251,15 @@ class ResNetGenerator(Model):
         x = self.downsample_8(x) # (bs, 1, 1, 512)
 
         # refer to https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
+
+
+
+def get_norm_layer(norm_type):
+    if norm_type.lower() == "batchnorm":
+        return BatchNormalization()
+    elif norm_type.lower() == "instancenorm":
+        return InstanceNormalization()
+    else:
+        raise ValueError("arg `norm_type` has to be either batchnorm "
+                         "or instancenorm. What you specified is "
+                         "{}".format(norm_type))
