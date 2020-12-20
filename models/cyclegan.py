e13dae34bcf3343d44381095cc8bf5246e010f34
Masao-Taketani <taket003@umn.edu>
2020-12-21 02:30:45 +0900

    init func is done for ResNetGenerator

--- models/cyclegan.py
+++ models/cyclegan.py
@@ -3,6 +3,7 @@ from tensorflow.keras import Sequential
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Layer, Conv2D, BatchNormalization, LeakyReLU\
     ReLU, Conv2DTranspose, Dropout, concatenate, ZeroPadding2D
+from tensorflow.keras.activation import tanh
 
 
 class InstanceNormalization(Layer):
@@ -38,7 +39,13 @@ class InstanceNormalization(Layer):
 class ResNetBlock(Layer):
     # Define ResNet block
 
-    def __init__(self, filters, size, strides, padding="VALID", name="resnet_block"):
+    def __init__(self,
+                 filters, 
+                 size=3, 
+                 strides=1, 
+                 padding="VALID", 
+                 name="resnetblock"):
+
         super(ResNetBlock, self).__init__()
         initializer = tf.random_normal_initializer(0., 0.02)
         self.kernel_size = kernel_size
@@ -46,27 +53,52 @@ class ResNetBlock(Layer):
                                size, 
                                strides, 
                                padding=padding,
-                               kernel_initializer=initializer)
+                               kernel_initializer=initializer,
+                               use_bias=)
         self.instance_norm_1 = InstanceNormalization()
+        self.ReLU = ReLU()
         self.conv2d_2 = Conv2D(num_filters,
                                kernel_size,
                                strides,
                                padding=padding,
-                               kernel_initializer=initializer)
+                               kernel_initializer=initializer,
+                               use_bias=)
         self.instance_norm_2 = InstanceNormalization()
 
         
     def call(inputs):
         pad = int((kernel_size - 1) / 2)
+        # Reflection padding was used to reduce artifacts
         x = tf.pad(inputs, [[0, 0], [pad, pad], [pad, pad], [0, 0]], "REFLECT")
         x = self.conv2d_1(x)
         x = self.instance_norm_1(x)
-        x = ReLU(x)
+        x = self.ReLU(x)
         x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]], "REFLECT")
         x = self.conv2d_2(x)
         x = self.instance_norm_2(x)
         return x + inputs
 
+
+def get_norm_layer(norm_type):
+    if norm_type.lower() == "batchnorm":
+        return BatchNormalization()
+    elif norm_type.lower() == "instancenorm":
+        return InstanceNormalization()
+    else:
+        raise ValueError("arg `norm_type` has to be either batchnorm "
+                         "or instancenorm. What you specified is "
+                         "{}".format(norm_type))
+
+
+def get_activation(activation):
+    if activation.lower() == "relu":
+        return ReLU()
+    elif activation.lower() == "tanh":
+        return tanh
+    else:
+        raise ValueError("arg `norm_type` has to be either relu "
+                         "or tanh. What you specified is "
+                         "{}".format(norm_type))
 
 
 class Downsample(Layer):
@@ -85,7 +117,9 @@ class Downsample(Layer):
 
     def __init__(self, 
                  filters, 
-                 size, 
+                 size,
+                 strides=1,
+                 padding="same",
                  norm_type="batchnorm", 
                  name="downsample", 
                  **kwargs):
@@ -97,15 +131,17 @@ class Downsample(Layer):
             self.norm_layer = get_norm_layer(norm_type)
         self.conv2d = Conv2D(filters,
                              size,
-                             strides=2,
-                             padding="same",
+                             strides=strides,
+                             padding=padding,
                              kernel_initializer=initializer,
                              use_bias=False)
+        self.relu = ReLU()
 
     def call(self, inputs):
         x = self.conv2d(inputs)
         if self.norm_type:
             x = self.norm_layer(x)
+        x = self.relu(x)
 
         return x
 
@@ -126,9 +162,12 @@ class Upsample(Layer):
     """
     def __init__(self, 
                  filters, 
-                 size, 
+                 size,
+                 strides,
+                 padding,
                  norm_type="batchnorm",
-                 apply_dropout=False, 
+                 apply_dropout=False,
+                 activation="relu",
                  name="upsample", 
                  **kwargs):
 
@@ -140,11 +179,13 @@ class Upsample(Layer):
         self.apply_dropout = apply_dropout
         self.conv2dtranspose = Conv2DTranspose(filters,
                                                size,
-                                               strides=2,
-                                               padding="same",
+                                               strides=strides,
+                                               padding=padding,
                                                kernel_initializer=initializer,
                                                use_bias=False)
-        self.dropout = Dropout(0.5)
+        if apply_dropout:
+            self.dropout = Dropout(0.5)
+        self.activation = get_activation(activation)
 
     def call(self, inputs):
         x = self.conv2dtranspose(inputs)
@@ -152,6 +193,7 @@ class Upsample(Layer):
             x = self.norm_layer(x)
         if self.apply_dropout:
             x = self.dropout(x)
+        x = self.activation(x)
 
         return x
 
@@ -198,7 +240,7 @@ class Discriminator(Model):
 
 
 
-class ResNetGenerator(Model):
+class Pix2Pix(Model):
     """
     Args:
     output_channels: number of output channels
@@ -238,7 +280,7 @@ class ResNetGenerator(Model):
                                            padding="same",
                                            kernel_initializer=initializer,
                                            activation="tanh")
-                                
+
     def call(self, inputs):
         # input shape: (bs, 256, 256, 3)
         x = self.downsample_1(inputs) # (bs, 128, 128, 64)
@@ -251,15 +293,90 @@ class ResNetGenerator(Model):
         x = self.downsample_8(x) # (bs, 1, 1, 512)
 
         # refer to https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
+    
 
+  class ResNetGenerator(Model):
+    """
+    Referred from CycleGAN paper(https://arxiv.org/abs/1703.10593).
+    Let c7s1-k denote a 7×7 Convolution-InstanceNormReLU layer with k filters 
+    and stride 1. dk denotes a 3 × 3 Convolution-InstanceNorm-ReLU layer with 
+    k filters and stride 2. Reflection padding was used to reduce artifacts.
+    Rk denotes a residual block that contains two 3 × 3 convolutional layers 
+    with the same number of filters on both layer. 
+    uk denotes a 3 × 3 fractional-strided-ConvolutionInstanceNorm-ReLU layer 
+    with k filters and stride 1/2.
 
+    The network with 6 residual blocks consists of:
+        c7s1-64,d128,d256,R256,R256,R256,
+        R256,R256,R256,u128,u64,c7s1-3
 
-def get_norm_layer(norm_type):
-    if norm_type.lower() == "batchnorm":
-        return BatchNormalization()
-    elif norm_type.lower() == "instancenorm":
-        return InstanceNormalization()
-    else:
-        raise ValueError("arg `norm_type` has to be either batchnorm "
-                         "or instancenorm. What you specified is "
-                         "{}".format(norm_type))
+    The network with 9 residual blocks consists of:
+        c7s1-64,d128,d256,R256,R256,R256,
+        R256,R256,R256,R256,R256,R256,u128
+        u64,c7s1-3
+
+    Args:
+    output_channels: number of output channels
+          norm_type: normalization type. Either "batchnorm", "instancenorm" or None
+
+    Return:
+        Generator model
+    """
+    def __init__(self,
+                 first_filters,
+                 output_channels,
+                 norm_type="instancenorm", 
+                 name="resnet_generator"):
+
+        super(ResNetGenerator, self).__init__(name=name, **kwargs)
+        initializer = tf.random_normal_initializer(0., 0.02)
+        self.downsample_1 = Downsample(first_filters, 
+                                       7,
+                                       strides=1,
+                                       padding="VALID",
+                                       norm_type="instancenorm", 
+                                       name="downsample_1",
+                                       use_bias=)
+        self.downsample_2 = Downsample(first_filters*2, 
+                                       3,
+                                       strides=2,
+                                       norm_type="instancenorm", 
+                                       name="downsample_2",
+                                       use_bias=))
+        self.downsample_3 = Downsample(first_filters*4, 
+                                       3,
+                                       strides=2,
+                                       norm_type="instancenorm", 
+                                       name="downsample_2",
+                                       use_bias=))
+        self.resnetblock_1 = ResNetBlock(first_filters*4, name="resnetblock_1")
+        self.resnetblock_2 = ResNetBlock(first_filters*4, name="resnetblock_2")
+        self.resnetblock_3 = ResNetBlock(first_filters*4, name="resnetblock_3")
+        self.resnetblock_4 = ResNetBlock(first_filters*4, name="resnetblock_4")
+        self.resnetblock_5 = ResNetBlock(first_filters*4, name="resnetblock_5")
+        self.resnetblock_6 = ResNetBlock(first_filters*4, name="resnetblock_6")
+        self.resnetblock_7 = ResNetBlock(first_filters*4, name="resnetblock_7")
+        self.resnetblock_8 = ResNetBlock(first_filters*4, name="resnetblock_8")
+        self.resnetblock_9 = ResNetBlock(first_filters*4, name="resnetblock_9")
+        self.upsample_1 = Upsample(first_filters*2, 
+                                   3,
+                                   2,
+                                   padding,
+                                   norm_type="intancenorm",
+                                   name="upsample_1")
+        self.upsample_2 = Upsample(first_filters, 
+                                   3,
+                                   2,
+                                   padding,
+                                   norm_type="intancenorm",
+                                   name="upsample_2")
+        self.upsample_3 = Upsample(output_channels, 
+                                   3,
+                                   2,
+                                   padding,
+                                   norm_type=None,
+                                   activation="tanh",
+                                   name="upsample_3")
+
+    def call(self, inputs):
+        # Reflection padding was used to reduce artifacts
